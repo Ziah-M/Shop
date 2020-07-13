29bc0dd2fd45e5bce0885d6720aa8ec0ae4457fe
Ziah-M <65767717+Ziah-M@users.noreply.github.com>
2020-07-14 08:46:22 +1000

    Add Products page

--- Pages/Products/Products.js
+++ Pages/Products/Products.js
@@ -1,14 +1,49 @@
-import React from 'react'
-import CategoriesSidebar from './CategoriesSidebar'
-import Card from './Card'
+import React, { useEffect } from "react";
+import CategoriesSidebar from "./CategoriesSidebar";
+import Card from "./Card";
+import { Container, Row, Col, Button } from "react-bootstrap";
 
-const Products = () => {
-    return (
-        <div>
-            <CategoriesSidebar />
-            <Card />
-        </div>
-    )
-}
+const DEFAULT_PRODUCTS = [];
 
-export default Products
+const Products = ({
+  products = DEFAULT_PRODUCTS,
+  categories,
+  addToCart = (f) => f,
+  removeFromCart = (f) => f,
+  selectedCategory,
+  setSelectedCategory = (f) => f,
+  getCartQty = (f) => f,
+}) => {
+  // TODO
+  // Should 1. Filter out products not matching selectedCategory
+  // 2. map over the products and append the cartQty using getCartQty(id)
+  const filteredProducts = products;
+  return (
+    <Container fluid>
+      <Row>
+        <Col lg={3} md={4} sm={9} xs={12}>
+          <CategoriesSidebar
+            categories={categories}
+            selectedCategory={selectedCategory}
+            setSelectedCategory={setSelectedCategory}
+          />
+        </Col>
+        <Col>
+          <Row>
+            {filteredProducts.map((product, index) => (
+              <Col key={index} lg={3} md={6}>
+                <Card
+                  addToCart={addToCart}
+                  removeFromCart={removeFromCart}
+                  product={product}
+                />
+              </Col>
+            ))}
+          </Row>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Products;
