39e584d526cd6df5f0a7f5161dbf7d944f62d5d6
Ziah-M <65767717+Ziah-M@users.noreply.github.com>
2020-07-14 13:35:54 +1000

    Order Service placeholder

--- Services/withOrderService.js
+++ Services/withOrderService.js
@@ -1,16 +1,61 @@
 import React, { Component } from "react";
+import { withFirebase } from "../../../shared/Firebase";
+import { withRouter } from "react-router-dom";
+import * as ROUTES from "../Constants/routes";
+import withCartService from "./withCartService";
+import { compose } from "recompose";
+import withAuthentication from "../../../shared/Session/withAuthentication";
+
+const DEFAULT_ORDER_MODEL = {
+  // TODOO Add datePlaced when generating a new order
+  userId: 0,
+  shipping: {},
+  cartItems: [],
+};
+
+const INITIAL_STATE = {
+  myOrders: [],
+};
 
 const withOrderService = (ComposedComponent) => {
   class WithOrderService extends Component {
-      constructor(props) {
-          super(props)
-      }
-      
+    constructor(props) {
+      super(props);
+
+      this.state = INITIAL_STATE;
+
+      this.placeOrder = this.placeOrder.bind(this);
+      this.getOrdersByUser = this.getOrdersByUser.bind(this);
+      this.getAllOrders = this.getAllOrders.bind(this);
+    }
+
+    placeOrder(shipping = {}) {
+    }
+
+    getOrdersByUser(userId) {
+    }
+
+    getAllOrders() {
+    }
+
     render() {
-      return <ComposedComponent {...this.props}/>;
+      return (
+        <ComposedComponent
+          {...this.props}
+          {...this.state}
+          placeOrder={this.placeOrder}
+          getOrdersByUser={this.getOrdersByUser}
+          getAllOrders={this.getAllOrders}
+        />
+      );
     }
   }
-  return WithOrderService
+  return compose(
+    withFirebase,
+    withCartService,
+    withRouter,
+    withAuthentication
+  )(WithOrderService);
 };
 
 export default withOrderService;
