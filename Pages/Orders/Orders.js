8492b4e54570a26167558c8a0036501addacfcc2
Ziah-M <65767717+Ziah-M@users.noreply.github.com>
2020-07-14 13:08:45 +1000

    Placeholder Orders Page

--- Pages/Orders/Orders.js
+++ Pages/Orders/Orders.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from "react";
+import { Container, Table, Nav } from "react-bootstrap";
+import { withOrderService } from "../../Services";
+import { withRouter } from "react-router-dom";
 
-const Orders = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+const DEFAULT_ORDERS = [
+  { shipping: { name: "testOrder1" }, datePlaced: "testDate1" },
+  { shipping: { name: "testOrder2" }, datePlaced: "testDate2" },
+];
 
-export default Orders
+const Orders = ({ orders = DEFAULT_ORDERS }) => {
+  return (
+    <Container>
+      <h1>Orders</h1>
+      <Table>
+        <thead>
+          <tr>
+            <th>Customer</th>
+            <th>Date</th>
+            <th></th>
+          </tr>
+          <tbody>
+            {Orders.map((order) => (
+              <tr>
+                <td>{order.shipping.name}</td>
+                <td>{order.datePlaced}</td>
+                <td>
+                  <Nav.Link>View</Nav.Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </thead>
+      </Table>
+    </Container>
+  );
+};
+
+export default withOrderService(withRouter(Orders));
