70a3cb4dec08ec09c9d0a3cc09fc9c8da7a02163
Ziah-M <65767717+Ziah-M@users.noreply.github.com>
2020-07-14 13:00:23 +1000

    Add CheckOut Page, Shipping Form

--- Pages/CheckOut/CheckOut.js
+++ Pages/CheckOut/CheckOut.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+import { Container, Col, Row } from "react-bootstrap";
+import CartSummary from "../Cart/CartSummary";
+import Shipping from "../Shipping";
 
 const CheckOut = () => {
-    return (
-        <div>
-            Checkout
-        </div>
-    )
-}
+  return (
+    <Container>
+      <Row>
+        <h2>Shipping</h2>
+      </Row>
+      <Row>
+        <Col xs={6}>
+          <Shipping />
+        </Col>
+        <Col xs={6}>
+          <CartSummary />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
-export default CheckOut
+export default CheckOut;
--- Pages/Shipping/Form.js
+++ Pages/Shipping/Form.js
@@ -1,8 +1,84 @@
-import React from "react";
-import { Form } from "react-bootstrap";
+import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+import { withOrderService } from "../../Services";
 
-const ShippingForm = () => {
-  return <Form></Form>;
+const DEFAULT_SHIPPING = {
+  addressLine1: "",
+  addressLine2: "",
+  name: "",
+  city: "",
 };
 
-export default ShippingForm;
+const ShippingForm = ({ placeOrder = (f) => f }) => {
+  const [formIsValid, setFormIsValid] = useState(false);
+  const [shipping, setShipping] = useState(DEFAULT_SHIPPING);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+      console.log("FOR WAS INVALID!");
+    }
+    // TODO --- placeOrder(shipping)} valid={formIsValid}
+    // TODO Input fields to create shipping object
+    setFormIsValid(true);
+    console.log("FORM WAS VALID!");
+  };
+
+  return (
+    <Form submit={handleSubmit}>
+      <Form.Group id="input name">
+        <Form.Label>Name</Form.Label>
+        <Form.Control type="text" name="name" required></Form.Control>
+        <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+        <Form.Control.Feedback type="invalid">
+          Name is required.
+        </Form.Control.Feedback>
+      </Form.Group>
+
+      <Form.Group id="input Address Line 1">
+        <Form.Label>Address</Form.Label>
+        <Form.Control
+          required
+          name="addressLine1"
+          type="text"
+          placeholder="Line 1"
+        ></Form.Control>
+        <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+        <Form.Control.Feedback type="invalid">
+          Please provide line 1 of address.
+        </Form.Control.Feedback>
+      </Form.Group>
+
+      <Form.Group id="input Address Line 2">
+        <Form.Label>Address</Form.Label>
+        <Form.Control
+          name="addressLine2"
+          type="text"
+          placeholder="Line 2"
+          required
+        ></Form.Control>
+        <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+        <Form.Control.Feedback type="invalid">
+          Please provide line 2 of address.
+        </Form.Control.Feedback>
+      </Form.Group>
+
+      <Form.Group id="input City">
+        <Form.Label>City</Form.Label>
+        <Form.Control name="city" type="text" required></Form.Control>
+        <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+        <Form.Control.Feedback type="invalid">
+          Please provide a valid city.
+        </Form.Control.Feedback>
+      </Form.Group>
+
+      <Button id="Submit Order" variant="primary" type="submit">
+        Place Order
+      </Button>
+    </Form>
+  );
+};
+
+export default withOrderService(ShippingForm);
